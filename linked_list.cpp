146f6af70d60731dcc2c6fd378bdb9e8bca3ff5a
Priyanka pawar <priyanka.pawar30@gmail.com>
2019-06-02 01:12:07 +0530

    adding delete list, insertatfront

--- linked_list.cpp
+++ linked_list.cpp
@@ -9,7 +9,10 @@ struct Node{
 
 void insertNodeAtHead(struct Node**);
 
-struct Node* createLinkedList(int size){
+struct Node* createLinkedList(){
+	cout<<"Enter the size of linked list: ";
+	int size = 0;
+	cin>>size;
 	struct Node* head = NULL;
 	while(size){
 		insertNodeAtHead(&head);
@@ -25,6 +28,55 @@ void insertNodeAtHead(struct Node** head){
 	*head = new_node;
 
 }
+void insertAt(struct Node** head, int pos, int value){
+	struct Node* temp = *head;
+	int c=1;
+	struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+	new_node->data = value;
+	new_node->next = NULL;
+	if(pos==1){
+		new_node->next = *head;
+		*head = new_node;
+		return;
+
+	}
+    while(c<pos-1 && temp){
+    	temp=temp->next;
+    	c++;
+    }
+
+    if(temp==NULL){
+    	cout<<"position doesn't exist<<endl";
+    }else{
+    	new_node->next=temp->next;
+    	temp->next=new_node;
+
+    }
+
+
+}
+
+int deleteNodeFirst(struct Node** head){
+	struct Node* temp = *head;
+	if(temp==NULL){
+		return -1;
+	}
+    *head = temp->next;
+    int deletedData = temp->data;
+     free(temp);
+     return deletedData;
+}
+void deleteList(struct Node** head){
+	struct Node* temp;
+	struct Node* curr = *head;
+	while(*head){
+		temp=curr;
+		*head = (*head)->next;
+		curr = *head;
+		free(temp);
+
+	}
+}
 void traverse(struct Node* head){
 	cout<<"Traversing Linked list...."<<endl;
 	struct Node* temp=head;
@@ -32,15 +84,52 @@ void traverse(struct Node* head){
 		cout<<"Node - "<<temp->data<<endl;
 		temp=temp->next;
 	}
-}
+}		
 int main()
 {
-	cout<<"Enter size of linked list: ";
-	int size = 0;
-	cin>>size;
-	struct Node* head = createLinkedList(size);
-	traverse(head);
-
+	int choice, pos, value;
+	bool loop = true;
+	struct Node* head = NULL;
+	while(loop){
+		cout<<"Linked List Menu : "<<endl;
+		cout<<"Press 1 to create : "<<endl;
+		cout<<"Press 2 to traverse : "<<endl;
+		cout<<"Press 3 to delete front node in : "<<endl;
+		cout<<"Press 4 to insert at a position : "<<endl;
+		cout<<"Press 5 to delete whole linked list : "<<endl;
+		cout<<"Press 9 to exit : "<<endl;
+		cout <<"Enter your choice - ";
+		cin>>choice;
+		switch(choice){
+			case 1:
+				if(head){
+					cout<<"linked list already created";
+				}else{
+					head = createLinkedList();
+				}
+				break;
+			case 2:
+				traverse(head);
+				break;
+			case 3:
+				deleteNodeFirst(&head);
+				break;
+			case 4:
+				cout<<"enter value and position";
+				cin>>value>>pos;
+				insertAt(&head,pos,value);
+				break;
+			case 5:
+				deleteList(&head);
+				break;
+			case 9:
+				loop = false;
+				break;
+			default:
+				cout<<"enter valid choice";
+				break;
+		}
+	}
 	return 1;
 }
 
